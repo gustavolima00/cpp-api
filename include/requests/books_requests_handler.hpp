924da9a12ad9aabf74fb76d2aa08c9bb5444ace9
Gustavo Marques <gustavomarques.gml@gmail.com>
2023-09-14 12:14:44 -0300

    Creation of books_requests_handler

---
+++ include/requests/books_requests_handler.hpp
@@ -0,0 +1,130 @@
+#pragma once
+
+#include <iostream>
+
+#include <restinio/all.hpp>
+#include <json_dto/pub.hpp>
+#include "models/book.hpp"
+#include "repositories/books_repository.hpp"
+#include "requests/base_request_handler.hpp"
+
+using book_collection_t = std::vector<Book>;
+
+namespace rr = restinio::router;
+using router_t = rr::express_router_t<>;
+
+class BooksRequestsHandler : BaseRequestHandler
+{
+public:
+  explicit BooksRequestsHandler()
+  {
+    BooksRepository &books_repository = BooksRepository::getInstance();
+  }
+
+  BooksRequestsHandler(const BooksRequestsHandler &) = delete;
+  BooksRequestsHandler(BooksRequestsHandler &&) = delete;
+
+  auto on_books_list(
+      const restinio::request_handle_t &req, rr::route_params_t) const
+  {
+    try
+    {
+      auto books = books_repository.get_books();
+      auto response = init_response(req->create_response(), "application/json");
+      return return_as_json(response, books);
+    }
+    catch (const std::exception &ex)
+    {
+      auto response = init_response(req->create_response(), "plain/text");
+      return return_internal_server_error(response, ex);
+    }
+  }
+
+  auto on_book_get(const restinio::request_handle_t &req, rr::route_params_t params)
+  {
+    try
+    {
+      const auto id = restinio::cast_to<std::uint32_t>(params["id"]);
+      auto book = books_repository.get_book(id);
+
+      auto response = init_response(req->create_response(), "application/json");
+      return return_as_json(response, book);
+    }
+    catch (const std::exception &ex)
+    {
+      auto response = init_response(req->create_response(), "plain/text");
+      return return_internal_server_error(response, ex);
+    }
+  }
+
+  auto on_author_get(
+      const restinio::request_handle_t &req, rr::route_params_t params)
+  {
+    try
+    {
+      auto author = restinio::utils::unescape_percent_encoding(params["author"]);
+      auto books = books_repository.get_by_author(author);
+
+      auto response = init_response(req->create_response(), "application/json");
+      return return_as_json(response, books);
+    }
+    catch (const std::exception &ex)
+    {
+      auto response = init_response(req->create_response(), "plain/text");
+      return return_internal_server_error(response, ex);
+    }
+  }
+
+  auto on_new_book(
+      const restinio::request_handle_t &req, rr::route_params_t)
+  {
+    try
+    {
+      Book new_book = json_dto::from_json<Book>(req->body());
+      books_repository.add_book(new_book);
+      auto response = init_response(req->create_response(), "application/json");
+      return return_as_json(response, new_book);
+    }
+    catch (const std::exception &ex)
+    {
+      auto response = init_response(req->create_response(), "plain/text");
+      return return_internal_server_error(response, ex);
+    }
+  }
+
+  auto on_book_update(
+      const restinio::request_handle_t &req, rr::route_params_t params)
+  {
+    try
+    {
+      const auto id = restinio::cast_to<std::uint32_t>(params["id"]);
+      Book book = json_dto::from_json<Book>(req->body());
+      books_repository.update_book(book);
+      auto response = init_response(req->create_response(), "application/json");
+      return return_as_json(response, book);
+    }
+    catch (const std::exception &ex)
+    {
+      auto response = init_response(req->create_response(), "plain/text");
+      return return_internal_server_error(response, ex);
+    }
+  }
+
+  auto on_book_delete(
+      const restinio::request_handle_t &req, rr::route_params_t params)
+  {
+    try
+    {
+      const auto id = restinio::cast_to<std::uint32_t>(params["id"]);
+      books_repository.delete_book(id);
+    }
+    catch (const std::exception &ex)
+    {
+      auto response = init_response(req->create_response(), "plain/text");
+      return return_internal_server_error(response, ex);
+    }
+  }
+
+private:
+  BooksRepository books_repository;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <json_dto/pub.hpp>
 #include "models/book.hpp"
 #include "repositories/books_repository.hpp"
-#include "requests/base_request_handler.hpp"
+#include "requests/books_requests_handler.hpp"
 #include <functional>
 
 using book_collection_t = std::vector<Book>;
@@ -12,126 +12,10 @@ using book_collection_t = std::vector<Book>;
 namespace rr = restinio::router;
 using router_t = rr::express_router_t<>;
 
-class books_handler_t : BaseRequestHandler
-{
-public:
-  explicit books_handler_t()
-  {
-    BooksRepository &books_repository = BooksRepository::getInstance();
-  }
-
-  books_handler_t(const books_handler_t &) = delete;
-  books_handler_t(books_handler_t &&) = delete;
-
-  auto on_books_list(
-      const restinio::request_handle_t &req, rr::route_params_t) const
-  {
-    try
-    {
-      auto books = books_repository.get_books();
-      auto response = init_response(req->create_response(), "application/json");
-      return return_as_json(response, books);
-    }
-    catch (const std::exception &ex)
-    {
-      auto response = init_response(req->create_response(), "plain/text");
-      return return_internal_server_error(response, ex);
-    }
-  }
-
-  auto on_book_get(const restinio::request_handle_t &req, rr::route_params_t params)
-  {
-    try
-    {
-      const auto id = restinio::cast_to<std::uint32_t>(params["id"]);
-      auto book = books_repository.get_book(id);
-
-      auto response = init_response(req->create_response(), "application/json");
-      return return_as_json(response, book);
-    }
-    catch (const std::exception &ex)
-    {
-      auto response = init_response(req->create_response(), "plain/text");
-      return return_internal_server_error(response, ex);
-    }
-  }
-
-  auto on_author_get(
-      const restinio::request_handle_t &req, rr::route_params_t params)
-  {
-    try
-    {
-      auto author = restinio::utils::unescape_percent_encoding(params["author"]);
-      auto books = books_repository.get_by_author(author);
-
-      auto response = init_response(req->create_response(), "application/json");
-      return return_as_json(response, books);
-    }
-    catch (const std::exception &ex)
-    {
-      auto response = init_response(req->create_response(), "plain/text");
-      return return_internal_server_error(response, ex);
-    }
-  }
-
-  auto on_new_book(
-      const restinio::request_handle_t &req, rr::route_params_t)
-  {
-    try
-    {
-      Book new_book = json_dto::from_json<Book>(req->body());
-      books_repository.add_book(new_book);
-      auto response = init_response(req->create_response(), "application/json");
-      return return_as_json(response, new_book);
-    }
-    catch (const std::exception &ex)
-    {
-      auto response = init_response(req->create_response(), "plain/text");
-      return return_internal_server_error(response, ex);
-    }
-  }
-
-  auto on_book_update(
-      const restinio::request_handle_t &req, rr::route_params_t params)
-  {
-    try
-    {
-      const auto id = restinio::cast_to<std::uint32_t>(params["id"]);
-      Book book = json_dto::from_json<Book>(req->body());
-      books_repository.update_book(book);
-      auto response = init_response(req->create_response(), "application/json");
-      return return_as_json(response, book);
-    }
-    catch (const std::exception &ex)
-    {
-      auto response = init_response(req->create_response(), "plain/text");
-      return return_internal_server_error(response, ex);
-    }
-  }
-
-  auto on_book_delete(
-      const restinio::request_handle_t &req, rr::route_params_t params)
-  {
-    try
-    {
-      const auto id = restinio::cast_to<std::uint32_t>(params["id"]);
-      books_repository.delete_book(id);
-    }
-    catch (const std::exception &ex)
-    {
-      auto response = init_response(req->create_response(), "plain/text");
-      return return_internal_server_error(response, ex);
-    }
-  }
-
-private:
-  BooksRepository books_repository;
-};
-
 auto server_handler()
 {
   auto router = std::make_unique<router_t>();
-  auto handler = std::make_shared<books_handler_t>();
+  auto handler = std::make_shared<BooksRequestsHandler>();
 
   auto by = [&](auto method)
   {
@@ -147,8 +31,8 @@ auto server_handler()
   };
 
   // Handlers for '/' path.
-  router->http_get("/", by(&books_handler_t::on_books_list));
-  router->http_post("/", by(&books_handler_t::on_new_book));
+  router->http_get("/", by(&BooksRequestsHandler::on_books_list));
+  router->http_post("/", by(&BooksRequestsHandler::on_new_book));
 
   // Disable all other methods for '/'.
   router->add_handler(
@@ -157,7 +41,7 @@ auto server_handler()
       "/", method_not_allowed);
 
   // Handler for '/author/:author' path.
-  router->http_get("/author/:author", by(&books_handler_t::on_author_get));
+  router->http_get("/author/:author", by(&BooksRequestsHandler::on_author_get));
 
   // Disable all other methods for '/author/:author'.
   router->add_handler(
@@ -167,13 +51,13 @@ auto server_handler()
   // Handlers for '/:id' path.
   router->http_get(
       R"(/:id(\d+))",
-      by(&books_handler_t::on_book_get));
+      by(&BooksRequestsHandler::on_book_get));
   router->http_put(
       R"(/:id(\d+))",
-      by(&books_handler_t::on_book_update));
+      by(&BooksRequestsHandler::on_book_update));
   router->http_delete(
       R"(/:id(\d+))",
-      by(&books_handler_t::on_book_delete));
+      by(&BooksRequestsHandler::on_book_delete));
 
   // Disable all other methods for '/:id'.
   router->add_handler(
