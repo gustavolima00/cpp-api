ec7f063ee874ecce9801bd1b136f3e46052a33b4
Gustavo Marques <gustavomarques.gml@gmail.com>
2023-09-16 11:53:02 -0300

    Docker configuration

--- api/src/main.cpp
+++ api/src/main.cpp
@@ -2,6 +2,8 @@
 
 #include <restinio/all.hpp>
 #include "requests/people_router.hpp"
+// #define PORT 8080
+#define PORT 80
 
 namespace rr = restinio::router;
 using router_t = rr::express_router_t<>;
@@ -29,6 +31,7 @@ int main()
     restinio::run(
         restinio::on_this_thread<traits_t>()
             .address("0.0.0.0")
+            .port(PORT)
             .request_handler(server_handler())
             .read_next_http_message_timelimit(10s)
             .write_http_response_timelimit(1s)
